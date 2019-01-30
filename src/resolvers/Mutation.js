2f87813567fed21b194f39cf0fa941f56cbd46f7
dario <sotodario3@gmaill.com>
2019-01-29 20:47:12 -0400

    First commt

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,12 @@
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info)
+        return item;
+    }
+};
+
+module.exports = Mutations;
